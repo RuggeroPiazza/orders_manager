267056f9050b10e437f3cc356f55f00f6a131b2d
Ruggero <52915668+RuggeroPiazza@users.noreply.github.com>
2021-04-15 23:55:44 +0100

    functions added

--- orders_manager/users/utils.py
+++ orders_manager/users/utils.py
@@ -25,3 +25,47 @@ def change_date_format(date):
     dt = datetime.strptime(str_date, '%Y-%m-%d').strftime('%d/%m/%Y')
     return dt
 
+
+days_per_month = {1: 31,  # {month : num of days, ... }
+                  2: 30,
+                  3: 31,
+                  4: 30,
+                  5: 31,
+                  6: 30,
+                  7: 31,
+                  8: 31,
+                  9: 30,
+                  10: 31,
+                  11: 30,
+                  12: 31}
+
+
+def next_day(day, month, year):
+    if day < days_per_month[month]:
+        day += 1
+    else:
+        day = 1
+        if month < 12:
+            month += 1
+        else:
+            month = 1
+            year += 1
+
+    str_date = str(day) + '/' + str(month) + '/' + str(year)
+    return str_date
+
+
+def prev_day(day, month, year):
+    if day > 1:
+        day -= 1
+    else:
+        if month > 1:
+            month -= 1
+            day = days_per_month[month]
+        else:
+            day = 31
+            month = 12
+            year -= 1
+
+    str_date = str(day) + '/' + str(month) + '/' + str(year)
+    return str_date
