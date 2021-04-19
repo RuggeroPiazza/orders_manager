8bb684d55bfbbd470c656793a25f2b5be606608f
Ruggero <52915668+RuggeroPiazza@users.noreply.github.com>
2021-04-19 22:48:58 +0100

    minor changes to home route

--- orders_manager/main/routes.py
+++ orders_manager/main/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, Blueprint
 from flask_login import login_required
 from orders_manager.models import Post
+from orders_manager.users.utils import next_day, days_per_month
+from datetime import date
 
 main = Blueprint('main', __name__)
 
@@ -8,12 +10,16 @@ main = Blueprint('main', __name__)
 @main.route("/home", methods=['GET', 'POST'])
 @login_required
 def home():
+    today = date.today().strftime("%d/%m/%Y")
     page = request.args.get('page', 1, type=int)
-    #posts = Post.query.paginate(page=page, per_page=5)
-    #posts = Post.query.filter_by(date='15/04/2021').paginate(page=page, per_page=5)
-    posts = Post.query.order_by(Post.date.asc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    posts = Post.query.filter_by(date=today).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts, today=today)
 
+    # page = request.args.get('page', 1, type=int)
+    # #posts = Post.query.paginate(page=page, per_page=5)
+    # posts = Post.query.filter_by(date='13/04/2021').paginate(page=page, per_page=5)
+    # #posts = Post.query.order_by(Post.date.asc()).paginate(page=page, per_page=5)
+    # return render_template('home.html', posts=posts)
 
 @main.route("/about")
 def about():
