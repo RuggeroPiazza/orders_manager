61e71552c67edfe3d58da3d81bc382fb3b774059
Ruggero <52915668+RuggeroPiazza@users.noreply.github.com>
2021-04-21 21:49:46 +0100

    prev day function added to html

--- orders_manager/main/routes.py
+++ orders_manager/main/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, redirect
 from flask_login import login_required
 from orders_manager.models import Post
-from orders_manager.users.utils import next_day
+from orders_manager.users.utils import next_day, prev_day
 from datetime import datetime, date
 
 main = Blueprint('main', __name__)
@@ -26,9 +26,14 @@ def timeline(when):
     if when == 'next':
         new_date = next_day(main_date)
         main_date = datetime.strptime(new_date, '%d/%m/%Y')
-        page = request.args.get('page', 1, type=int)
-        posts = Post.query.filter_by(date=new_date).paginate(page=page, per_page=5)
-        return render_template('home.html', posts=posts, date=new_date)
+
+    if when == 'prev':
+        new_date = prev_day(main_date)
+        main_date = datetime.strptime(new_date, '%d/%m/%Y')
+
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.filter_by(date=new_date).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts, date=new_date)
 
 
 @main.route("/about")
